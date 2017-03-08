a98fd57d7b9f8a756e14519aecb380db65088115
Adan.Rabadan <evilb4k4@gmail.com>
2017-03-08 00:31:50 -0800

    late study

--- Aboutme.js
+++ Aboutme.js
@@ -1,15 +1,47 @@
 'use strict';
 
-var answer = prompt('Are you ready to rumble?');
+var answer = prompt('Do you like scary stories?');
 if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
-  // If the user answers yes, do the following.
-  alert('The user said they are indeed ready to rumble.');
+  alert('BOO! (it\'s a short story)');
 } else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
-  /*
-    If the user is not ready to rumble, do the following.
-  */
-  alert('The user is unfortunately not ready to rumble.');
+  alert('It\'s your loss.');
 } else {
-  // If the user can't read instructions properly, admonish them!
-  alert('You didn\'t put in a proper answer!');
+  alert('No need to get scared and start typing out control');
+}
+
+var answer = prompt('Are you ready to party?');
+if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
+  alert('OKAY! You go buy the Beer');
+}
+else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
+  alert('OK');
+} else {
+  alert('I\'ll take that as a no.');
+}
+
+var answer = prompt('Do you like Superman?');
+if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
+  alert('Superman is cool.');
+}else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
+  alert('Hater');
+} else {
+  alert('Simple yes or no question.');
+}
+
+var answer = prompt('Do you like tacos?');
+if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
+  alert('Tacos are awsome!');
+}else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
+  alert('More tacos for me then');
+} else {
+  alert('I\'ll take that as a no.');
+}
+
+var answer = prompt('Do you play video game?');
+if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
+  alert('Video games are my favorite pass time');
+} else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
+  alert('It okay we all can\'t be cool');
+} else {
+  alert('So maybe?');
 }
