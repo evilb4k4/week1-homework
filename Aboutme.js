d9e2716557d454e53fd8246ff452675114cfe1ac
Adan.Rabadan <evilb4k4@gmail.com>
2017-03-09 14:50:05 -0800

    saving Day 3

--- Aboutme.js
+++ Aboutme.js
@@ -1,68 +1,70 @@
 'use strict';
 
-// var answer = prompt('Do you like scary stories?');
-// if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
-//   alert('BOO! (it\'s a short story)');
-// } else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
-//   alert('It\'s your loss.');
-// } else {
-//   alert('No need to get scared and start typing out control');
-// }
-//
-// var answer = prompt('Are you ready to party?');
-// if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
-//   alert('OKAY! You go buy the Beer');
-// } else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
-//   alert('OK');
-// } else {
-//   alert('I\'ll take that as a no.');
-// }
-//
-// var answer = prompt('Do you like Superman?');
-// if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
-//   alert('Superman is cool.');
-// } else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
-//   alert('Hater');
-// } else {
-//   alert('Simple yes or no question.');
-// }
-//
-// var answer = prompt('Do you like tacos?');
-// if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
-//   alert('Tacos are awsome!');
-// } else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
-//   alert('More tacos for me then');
-// } else {
-//   alert('I\'ll take that as a no.');
-// }
-//
-// var answer = prompt('Do you play video game?');
-// if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
-//   alert('Video games are my favorite pass time');
-// } else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
-//   alert('It okay we all can\'t be cool');
-// } else {
-//   alert('So maybe?');
-// }
-// for (var x = 1; x <= 4; x++) {
-//   var  question6 = prompt('Can you guess how old I am?');
-//   console.log('it works');
-//   if (question6 === 31) {
-//     alert('You\'re spot on! I\'m 31 year old');
-//   } else if (question6 < 31) {
-//     alert('Too low');
-//   } else if (question6 > 31) {
-//     alert('Too high');
-//   }
-// }
-//
-// var answer = prompt('Can you guess what country other than USA I\'ve live in?');
-// var newArray = ['England','Mexico','Germany','Korea',];
-// for (var i = 0; i < newArray.length; i++){
-//   if (answer.toLowerCase() === newArray[i]) {
-//      alert('Correct !');
-//   } else  {
-//      alert('Nope');
-//   }
-//   continue;
-// }
+var answer = prompt('Do you like scary stories?');
+if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
+  alert('BOO! (it\'s a short story)');
+} else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
+  alert('It\'s your loss.');
+} else {
+  alert('No need to get scared and start typing out control');
+}
+
+var answer = prompt('Are you ready to party?');
+if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
+  alert('OKAY! You go buy the Beer');
+} else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
+  alert('OK');
+} else {
+  alert('I\'ll take that as a no.');
+}
+
+var answer = prompt('Do you like Superman?');
+if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
+  alert('Superman is cool.');
+} else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
+  alert('Hater');
+} else {
+  alert('Simple yes or no question.');
+}
+
+var answer = prompt('Do you like tacos?');
+if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
+  alert('Tacos are awsome!');
+} else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
+  alert('More tacos for me then');
+} else {
+  alert('I\'ll take that as a no.');
+}
+
+var answer = prompt('Do you play video game?');
+if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
+  alert('Video games are my favorite pass time');
+} else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
+  alert('It okay we all can\'t be cool');
+} else {
+  alert('So maybe?');
+}
+for (var x = 1; x < 4; x++) {
+  var  question6 = prompt('Can you guess how old I am?');
+  console.log('it works');
+  if (question6 === 31) {
+    alert('You\'re spot on! I\'m 31 year old');
+  } else if (question6 < 31) {
+    alert('Too low');
+  } else if (question6 > 31) {
+    alert('Too high');
+  }
+}
+
+for (var i = 0; i < newArray.length; i++){
+ var answer = prompt('Can you guess what country I\'ve travel to while in the military?');
+ var newArray = ['England','Mexico','Germany','Korea', 'Turkey', 'Russia'];
+ console.log('it works');
+ var modifiedAns = answer[0].toUpperCase() +answer.slice(1, answer.lenght-1).toLowerCase();
+   if (modifiedAns() === newArray[i]) {
+      alert('Correct !');
+      break;
+   } else  {
+      alert('Nope');
+   }
+ }
